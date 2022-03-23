7a80ca56a27b3de1f7e0b3ca7819445e3a4bde33
Dave Tisza <dtisza@fearless.tech>
2022-03-23 10:06:48 -0400

    Update package version/naming

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cms-bb2",
-    version="0.0.1",
+    version="1.0.0",
     author="CMS Blue Button 2.0 Team",
     author_email="bb2@example.com",  # TODO: Do we want to include?
     license="Apache Software License",
@@ -25,7 +25,7 @@ setup(
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    py_modules=["blueButton"],
+    py_modules=["bluebutton"],
     package_dir={"": "src"},
     python_requires=">=3.6",
     install_requires=[
