6ecf00805c1fc46975cd5b6c12777866667d19da
Dave Tisza <dtisza@fearless.tech>
2022-03-23 12:00:21 -0400

    Rename to cms_bluebutton

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="cms-bb2",
+    name="cms_bluebutton",
     version="1.0.0",
     author="CMS Blue Button 2.0 Team",
     author_email="bb2@example.com",  # TODO: Do we want to include?
@@ -25,7 +25,7 @@ setup(
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    py_modules=["bluebutton"],
+    py_modules=["cms_bluebutton"],
     package_dir={"": "src"},
     python_requires=">=3.6",
     install_requires=[
