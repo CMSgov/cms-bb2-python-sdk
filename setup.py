f57156c56570652f68f9cebe119f991605eb0064
David Tisza <36048547+dtisza1@users.noreply.github.com>
2022-04-26 10:16:19 -0400

    [ BB2-1259 ] Update license (#8)
    * Update license
    
    * Rename license file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version="1.0.0",
     author="CMS Blue Button 2.0 Team",
     author_email="bb2@example.com",  # TODO: Do we want to include?
-    license="Apache Software License",
+    license="CC0 1.0 Universal",
     description="An SDK used for interacting with the CMS Blue Button 2.0 API",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,7 +17,7 @@ setup(
     # For classifiers: https://pypi.org/classifiers/
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
