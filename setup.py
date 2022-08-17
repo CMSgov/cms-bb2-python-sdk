713e07bb9152375d3788e1a5e694060d22f4fb45
David Tisza <36048547+dtisza1@users.noreply.github.com>
2022-08-17 14:47:24 -0400

    [ BB2-1180 ] Prep needed to Publish (#16)
    * Update setup.py add requests-toolbelt
    
    * Update setup.py for dev
    
    * Add coverage RC config
    
    * Update MANIFEST.in
    
    * Update .gitignore with bb2_venv exclude
    
    * Remove un-needed MANIFEST.in
    
    * Update version and package naming
    
    * Update README w/ publishing instruction
    
    * Added some slight tweaks and updated version to test pypi upload
    
    Co-authored-by: Adrian Jones <adrian.jones@adhocteam.us>

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cms_bluebutton",
-    version="0.1.0",
+    name="cms-bluebutton-sdk",
+    version="0.1.2",
     author="CMS Blue Button 2.0 Team",
     author_email="bb2@example.com",  # TODO: Do we want to include?
     license="CC0 1.0 Universal",
@@ -29,11 +29,13 @@ setuptools.setup(
     python_requires=">=3.6",
     install_requires=[
         "requests >= 2.0",
+        "requests-toolbelt >= 0.9.1",
         "pyyaml >= 5.0",
     ],
     extras_require={
         "dev": [
             "pytest >= 6.0",
+            "requests-mock >= 1.9.3",
         ],
     },
 )
